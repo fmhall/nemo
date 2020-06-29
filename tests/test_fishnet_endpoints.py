3d644f0d97aa5884ffc78e3ab375a8f84094e70e
Mason <mason.hall@bloxroute.com>
2020-06-29 02:56:25 -0500

    started tests

---
+++ tests/test_fishnet_endpoints.py
@@ -0,0 +1,33 @@
+from typing import Dict
+
+from fastapi.testclient import TestClient
+
+from nemo.main import app
+
+client = TestClient(app)
+
+
+def test_acquire():
+    data = {
+        "fishnet": {"version": "1.15.7", "python": "2.7.11+", "apikey": "XXX"},
+        "stockfish": {
+            "name": "Stockfish 7 64",
+            "options": {"hash": "256", "threads": "4"},
+        },
+    }
+    r = client.post(
+        "/acquire",
+        json=data,
+    )
+    response = r.json()
+    assert r.status_code == 202
+    assert "work" in response
+
+    r2 = client.post(
+        "/acquire",
+        json=data,
+    )
+    response = r2.json()
+    assert r2.status_code == 204
+    assert "work" not in response
+
